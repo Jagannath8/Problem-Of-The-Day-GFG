9253a992eaae6875c3e27a32529d8ccdc21b9456
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-02-13 22:30:33 +0530

    Day 78

---
+++ Arithmetic Number.java
@@ -0,0 +1,12 @@
+class Solution{
+    int inSequence(int A, int B, int C){
+        if(A==B)
+            return 1;
+        
+        if((B>A && C>0) || (A>B && C<0))
+            if ((B-A)%C==0 && (B-A)/C>=0)
+                return 1;
+        
+        return 0;
+    }
+}
