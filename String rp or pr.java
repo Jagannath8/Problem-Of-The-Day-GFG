4c2be4b12be733a158ee1a61a0fe8d35a5084e95
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-03-22 21:58:01 +0530

    Day 115

---
+++ String rp or pr.java
@@ -0,0 +1,39 @@
+class Solution { 
+    static long solve(int X,int Y, String S){    
+        String s1="pr" ; String s2="rp";
+        if(X<Y){
+            int temp = X; X=Y; Y=temp;
+	        String t =s1; s1=s2; s2=t;
+        }
+        
+        long ans=0;
+        Stack<Character> st = new Stack<>();
+	    for(int i =S.length()-1; i>=0; i--){
+	    Character curr= S.charAt(i), first=s1.charAt(0), second = s1.charAt(1);
+	        if(st.size()>0 && curr ==first && st.peek()==second){
+	            st.pop();
+	            ans+=X;
+	        }
+	        else
+	            st.push(curr);
+	   }
+	   
+	   StringBuilder rem = new StringBuilder();
+	   while(st.size()>0)
+	        rem.append(st.pop());
+	   
+	    S = rem.toString();
+
+	   for(int i =S.length()-1; i>=0; i--){
+	       Character curr= S.charAt(i), first=s2.charAt(0), second = s2.charAt(1);
+	       if(st.size()>0 && curr ==first && st.peek()==second){
+	           st.pop();
+	           ans+=Y;
+	       }
+	       
+	       else
+	            st.push(curr);
+	   }
+	   return ans;
+	}
+}
