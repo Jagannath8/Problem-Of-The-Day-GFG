1d797f3e040e53655ff5bf7a8e0ea4031ea72d43
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-03-24 22:44:18 +0530

    Day 117

---
+++ Remove and Reverse.java
@@ -0,0 +1,50 @@
+class Solution { 
+    String removeReverse(String S) { 
+        // code here
+        StringBuffer s = new StringBuffer(S);
+        int freq[] = new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+        }
+    
+        int flag=0;
+        int st=0; int end=s.length()-1; 
+        while(st<=end){
+            if(flag==0){
+                if(freq[s.charAt(st)-'a']==1) 
+                    st++;
+            
+                else{
+                    freq[s.charAt(st)-'a']--;
+                    s.replace(st,st+1,"#");
+                    st++;
+                    flag=1; 
+                }
+            }
+            
+            else{
+                if(freq[s.charAt(end)-'a']==1) 
+                    end--;
+            
+                else{
+                    freq[s.charAt(end)-'a']--;
+                    s.replace(end,end+1,"#");
+                    end--;
+                    flag=0; 
+                }
+            }
+        }
+
+        if(flag==1)
+            s.reverse();
+        
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='#')
+                ans+=s.charAt(i);
+        }
+        
+        return ans;
+    }
+}
