52cf9e01267c0af0409aa6b494e353c2e858b31d
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-05-21 23:15:28 +0530

    Day 175

---
+++ Bus Conductor.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int findMoves(int n, int[] chairs, int[] passengers) {
+        // code here
+        Arrays.sort(chairs);
+        Arrays.sort(passengers);
+        
+        int count=0;
+        for(int i=0;i<n;i++){
+            count=count+(int)Math.abs(chairs[i]-passengers[i]);
+        }
+        return count;
+    }
+}
