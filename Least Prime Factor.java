cfa35e77c5a07850787dec8b96c1ffc3eba1c4dd
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-06-07 22:04:08 +0530

    Day 192

---
+++ Least Prime Factor.java
@@ -0,0 +1,20 @@
+class Solution{
+    public int[] leastPrimeFactor(int n){
+        int [] leastPrime = new int[n+1];
+        leastPrime[0] = 0;
+        leastPrime[1] = 1;
+        for(int i=2; i<=n; i++){
+            if(leastPrime[i] == 0){
+                int ind=1;
+                int j=i;
+                while(j*ind <= n){
+                    j = j*ind;
+                    ind++;
+                    if(leastPrime[j] == 0) leastPrime[j] = i;
+                    j= i;
+                }
+            }
+        }
+        return leastPrime;
+    }
+}
