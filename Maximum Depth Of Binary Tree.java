277f907165fcffa27e6ca59825d38bd312e122f0
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-06-28 22:04:37 +0530

    Day 213

---
+++ Maximum Depth Of Binary Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int maxDepth(Node root) {
+    // code here
+        return maxDepthRel(root);
+    }
+  
+    public static int maxDepthRel(Node root){
+        if(root==null) 
+            return 0;
+      
+        return 1+ Math.max(maxDepthRel(root.left),maxDepthRel(root.right));
+    }
+}
