2571d387001fde39a99b33b1dd82f48cd5057a49
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-07-08 22:02:16 +0530

    Day 223

---
+++ Find triplets with zero sum.java
@@ -0,0 +1,22 @@
+class Solution{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n){
+        //add code here.
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                if(arr[i]+arr[l]+arr[r]==0) 
+                    return true;
+                if(arr[i]+arr[l]+arr[r]<0) 
+                    l++;
+                else 
+                    r--;
+            }
+        }
+        return false;
+    }
+}
