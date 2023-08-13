17ab4da7786d0b2e47ac5cb2f268e86fd9854fc3
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-08-13 21:53:07 +0530

    Day 259

---
+++ Nth Fibonacci Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int nthFibonacci(int n){
+        // code here
+        if(n==0) 
+            return 0;
+        
+        int a =0, b=1;
+        int mod = (int)1e9 +7;
+        n-= 2;
+        while(n-- >=0) {
+            int c = (a%mod + b%mod) %mod;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
