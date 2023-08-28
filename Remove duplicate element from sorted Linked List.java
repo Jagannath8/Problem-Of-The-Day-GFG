060800faafe6e991524e2b435c27d11ec7d28ee7
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-08-28 21:54:39 +0530

    Day 274

---
+++ Remove duplicate element from sorted Linked List.java
@@ -0,0 +1,14 @@
+class GfG{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head){
+	    // Your code here
+    	Node curr=head;
+    	while(curr!=null && curr.next!=null){
+    	    if(curr.data==curr.next.data)
+    	        curr.next=curr.next.next;
+    	    else
+    	        curr=curr.next;
+    	}
+    	return head;
+    }
+}
