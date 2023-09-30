1aeac1384472fe59514eeae6d463132a900983de
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-09-30 21:49:20 +0530

    Day 307

---
+++ Boolean Matrix.java
@@ -0,0 +1,28 @@
+class Solution{
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(int matrix[][]){
+        // code here 
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int row[]=new int [r];   
+        Arrays.fill(row,0);
+        int col[]=new int[c];
+        Arrays.fill(col,0);
+        for(int i=0; i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==1){
+                    row[i]=1;         
+                    col[j]=1;
+                }
+            }
+        }
+       
+        for(int i=0; i<r;i++){
+            for(int j=0;j<c;j++){
+                if(row[i]==1 || col[j]==1)
+                    matrix[i][j]=1;
+            }
+        }
+    }
+}
