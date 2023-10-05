2be8462eaf0bfd6b58358120175eb749ac8014b1
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-10-05 22:08:02 +0530

    Day 312

---
+++ Count number of substrings.java
@@ -0,0 +1,26 @@
+class Solution{
+    long substrCount (String S, int K) {
+        // your code here
+        return atmost(S,K)-atmost(S,K-1);
+    }
+    
+    long atmost(String s, int k){
+        int arr[]=new int[26];
+        int dist=0;
+        int left=0;
+        long ans=0;
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            if(arr[s.charAt(i)-'a']==1)
+                dist++;
+            while(dist>k){
+                arr[s.charAt(left)-'a']--;
+                if(arr[s.charAt(left)-'a']==0)
+                    dist--;
+                left++;
+            }
+            ans+=(i-left+1);
+        }
+        return ans;
+    }
+}
