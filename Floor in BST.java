484b48bea9d5dcc6c1070110c06e0ecd8d9f4414
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-10-13 22:09:17 +0530

    Day 320

---
+++ Floor in BST.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int floor(Node root, int x) {
+        // Code here
+        int ans = -1;
+        while(root != null){
+            if(root.data <= x){
+                ans = root.data;
+                root = root.right;
+            }
+            else if(root.data > x){
+                root = root.left;
+            }
+        }
+        return ans;
+    }
+}
