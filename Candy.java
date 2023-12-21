ba29c67445d10a848067f6b0fe4febdd78666476
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2023-12-21 21:44:58 +0530

    Day 389

---
+++ Candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int minCandy(int n, int ratings[]) {
+        // code here
+        if(n==0)
+            return 0;
+        
+        int candies[]=new int[n];
+        Arrays.fill(candies,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                candies[i]=candies[i-1]+1;
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+                candies[i]=Math.max(candies[i], candies[i+1]+1);
+        }
+        
+        int ans=0;
+        for(int c:candies){
+            ans+=c;
+        }
+        return ans;
+    }
+}
